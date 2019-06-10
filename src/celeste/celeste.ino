380d0b7760c00a158a818b78c337ac59f636c961
Gustavo O. Mendonça <gustavo36@gmail.com>
2019-06-09 23:36:34 -0300

    merge code of motors manual control onto main

--- src/celeste/celeste.ino
+++ src/celeste/celeste.ino
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <WiFiUdp.h>
+#include "AccelStepper.h"
 
 
 #ifndef STASSID
@@ -10,6 +11,18 @@
 #endif
 
 #define API_TIMEOUT 10000
+
+
+String cmd;
+
+boolean newData = false;
+
+// Define some steppers and the pins the will use
+AccelStepper elevation(5, D0, D1, D2, D3);
+AccelStepper azimuth(5, D5, D6, D7, D8);
+
+long elevationTargetPosition;
+long azimuthTargetPosition;
 
 
 const char* ssid = STASSID;
@@ -138,6 +151,17 @@ void setCurrentTime() {
 void setup() {
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
+
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
+
+  
   
   connectWiFi();
   setCurrentTime();
@@ -145,7 +169,13 @@ void setup() {
   client.setFingerprint(fingerprint);
   client.setTimeout(API_TIMEOUT);
   delay(100);
+   
   
+  azimuth.setMaxSpeed(900.0);
+  azimuth.setAcceleration(1000.0);
+  
+  elevation.setMaxSpeed(900.0);
+  elevation.setAcceleration(1000.0);
   
   /*Serial.print("connecting to ");
   Serial.println(host);
@@ -202,4 +232,31 @@ void setup() {
 }
 
 void loop() {
+  while (Serial.available() > 0 ){
+    cmd = Serial.readString();
+  }
+  
+  elevationTargetPosition = elevation.targetPosition();
+  azimuthTargetPosition = azimuth.targetPosition();
+  
+  for (int i=0;i<cmd.length();i++) {Serial.println(7);
+    if (cmd[i]=='u') elevationTargetPosition += 56.88889;
+    else if (cmd[i]=='d') elevationTargetPosition -=56.88889;
+    else if (cmd[i]=='r') azimuthTargetPosition +=56.88889;
+    else if (cmd[i]=='l') azimuthTargetPosition -=56.88889;
+    else if (cmd[i]=='x') {
+      azimuth.setCurrentPosition(0);
+      elevation.setCurrentPosition(0);
+      azimuth.moveTo(0);
+      elevation.moveTo(0);
+
+    }
+  }
+  elevation.moveTo(elevationTargetPosition);
+  azimuth.moveTo(azimuthTargetPosition);
+  cmd = "";
+  while (elevation.distanceToGo()!=0 || azimuth.distanceToGo()!=0) {
+    elevation.run();
+    azimuth.run();
+  }
 }
